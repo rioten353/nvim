099759e82a955107f5de829e5f7ee48255073e1d
rioten <rioten353@gmail.com>
2024-10-29 10:04:28 +0000

    refacturing in options file

--- init.lua
+++ init.lua
@@ -12,4 +12,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+require("options")
 require("keymaps")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.background = "light"
-
-vim.opt.swapfile = false
-
 local map = vim.keymap.set
 
 map("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
