a8c8558c3ad6ca4e080e2a66019d2e17f3749283
root <rioten353@gmail.com>
2024-11-02 08:29:06 +0000

    code refacture

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,46 +1,33 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-      -- ensure_installed = { "lua_ls", "rust_analyzer" }
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			-- ensure_installed = { "lua_ls", "rust_analyzer" }
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      local lspconfig = require("lspconfig")
-      --  lspconfig.tsserver.setup({
-      --    capabilities = capabilities
-      --  })
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.dartls.setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
+			local servers = { "lua_ls", "dartls", "gopls", "rust_analyzer" }
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					capabilities = capabilities,
+				})
+			end
+		end,
+	},
 }
